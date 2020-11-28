43c7f9367d512787cb3014a48b42e9c74a0f5325
Steven Lance <slance@democracyprep.org>
2020-11-28 16:14:18 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,31 @@
+// 1. WARM UP: Create a while loop that counts to 10. Print out each value.
+
+
+
+
+// 2. HEATING UP: Create a while loop that prints out a word (like Hello!) 5 times.
+
+
+
+
+// 3. Count to 22, but this time print out a message that tells the user how many numbers they have counted to (E.g "I have counted to 1", "I have counted to 2", etc.)
+
+
+
+// 4. A numlist has been created for you. Use a while loop to add 10 random numbers to this list. HINT - Remember we can add to a list using .push()
+var numList = [];
+var random;
+
+
+
+// 5. Our favorite foods list is back! Print out each of the favorite foods using a while loop. HINT: Think about how you can use the .length property here! 
+var favFoods = ["Pizza", "Chicken", "Pasta", "Chocolate"];
+
+
+
+// 6. Make a list of your favorite TV shows. Use a while loop to print out a sentence saying each of your favorite shows (e.g. "I like to watch Stranger Things", "I like to watch Game of Thrones", etc.)
+
+
+
+
+// 7. Heads/Tails: Using a while loop and random numbers, make a program that flips a coin 10 times and prints out if you flipped heads or tails. Finally, return a message telling them how many times they filpped heads or tails.
