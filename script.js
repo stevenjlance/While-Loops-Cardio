ac3c7ba9d12a9fadb987510ed66b367443c99aeb
Steven Lance <44912347+stevenjlance@users.noreply.github.com>
2021-11-03 12:05:02 +0000

    Added problem

--- script.js
+++ script.js
@@ -22,52 +22,58 @@ counter = 0;
 
 
 
-// 4. A variable called random has been created. Use a while loop to print out 10 random numbers between 0 and 10. 
+// 4. Create a new while loop to give you all the powers of 2 (e.g. 2^0, 2^1, etc.) up to 10.
+
+
+
+
+
+// 5. A variable called random has been created. Use a while loop to print out 10 random numbers between 0 and 10. 
 // NOTE: Math.floor(Math.random()*11) returns a random number between 0 and 10. 
 counter = 0;
 var random;
 
 
-// 5. How could we modify what we did above and add all the numbers to an array? Remember .push adds a value to a list!
+// 6. How could we modify what we did above and add all the numbers to an array? Remember .push adds a value to a list!
 var randNumbers = [];
 counter = 0;
 
 
 
-// 6. Finally, we want to count up and tell the user if their number is even or odd. Count to 20 and print out a statement saying 1 is odd, 2 is even, etc. all the way to 20!
+// 7. Finally, we want to count up and tell the user if their number is even or odd. Count to 20 and print out a statement saying 1 is odd, 2 is even, etc. all the way to 20!
 counter = 0;
 
 
 
 
-
-// 7. Create a while loop that counts to 10. Print out each value.
-
-
-
-// 8. Create a while loop that prints out a word (like Hello!) 5 times.
+// ============= WHILE LOOPS CARDIO ==================
+// 8. Create a while loop that counts to 10. Print out each value.
 
 
 
-// 9. Count to 22, but this time print out a message that tells the user how many numbers they have counted to (E.g "I have counted to 1", "I have counted to 2", etc.)
+// 9. Create a while loop that prints out a word (like Hello!) 5 times.
+
+
+
+// 10. Count to 22, but this time print out a message that tells the user how many numbers they have counted to (E.g "I have counted to 1", "I have counted to 2", etc.)
 
 
 
 
-// 10. A numlist has been created for you. Use a while loop to add 10 random numbers to this list. HINT - Remember we can add to a list using .push()
+// 11. A numlist has been created for you. Use a while loop to add 10 random numbers to this list. HINT - Remember we can add to a list using .push()
 var numList = [];
 var random;
 
 
 
-// 11. Our favorite foods list is back! Print out each of the favorite foods using a while loop. HINT: Think about how you can use the .length property here! 
+// 12. Our favorite foods list is back! Print out each of the favorite foods using a while loop. HINT: Think about how you can use the .length property here! 
 var favFoods = ["Pizza", "Chicken", "Pasta", "Chocolate"];
 
 
 
-// 12. Make a list of your favorite TV shows. Use a while loop to print out a sentence saying each of your favorite shows (e.g. "I like to watch Stranger Things", "I like to watch Game of Thrones", etc.)
+// 13. Make a list of your favorite TV shows. Use a while loop to print out a sentence saying each of your favorite shows (e.g. "I like to watch Stranger Things", "I like to watch Game of Thrones", etc.)
 
 
 
 
-// 13. STRETCH: Heads/Tails: Using a while loop and random numbers, make a program that flips a coin 10 times and prints out if you flipped heads or tails. Finally, return a message telling them how many times they filpped heads or tails.
+// 14. STRETCH: Heads/Tails: Using a while loop and random numbers, make a program that flips a coin 10 times and prints out if you flipped heads or tails. Finally, return a message telling them how many times they filpped heads or tails.
